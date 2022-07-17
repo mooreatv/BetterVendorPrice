b748ade3a141b9be770d59c062a1ec59971de0ac
Moorea Tv <moorea@ymail.com>
2022-07-17 12:03:02 -0700

    Wrath Classic beta support as well as legacy

--- installbvp.bat
+++ installbvp.bat
@@ -1,4 +1,4 @@
-for %%x in (_retail_ _classic_ _classic_era_) do (
+for %%x in (_retail_ _classic_ _classic_era_ _classic_beta_ legacy) do (
 echo Installing for %%x
 xcopy /i /y BetterVendorPrice\*.* "C:\Program Files (x86)\World of Warcraft\%%x\Interface\Addons\BetterVendorPrice"
 xcopy /i /y BetterVendorPrice\locale\*.* "C:\Program Files (x86)\World of Warcraft\%%x\Interface\Addons\BetterVendorPrice\locale"
